52023a2f34e791a6f71fb13e7db235490fe2d586
Nelson Rivera <rd12004@outlook.com>
2017-01-29 17:16:07 -0600

    Galería de Imágenes

--- app/Imagen.php
+++ app/Imagen.php
@@ -9,6 +9,22 @@ class Imagen extends Model
     /**
      * @var string
      */
-    public $table='clientes';
+    public $table='imagenes';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title', 'description','file_name','default',
+    ];
+
+    /**
+     * Obtiene la imagen por defecto para sucursales y logos
+     */
+    public static function getDefaultImage(){
+        return Imagen::where('default',true)->first();
+    }
 
 }
